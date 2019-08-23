d1e19b52cd2686e238dfd7d4078a077885b619d6
Anthony Wilkey <groovykool@gmail.com>
2019-08-23 16:36:21 -0500

    Added mute buttons

--- BasicVLC/MainPage.xaml.cs
+++ BasicVLC/MainPage.xaml.cs
@@ -21,74 +21,92 @@ using System.Threading;
 
 namespace BasicVLC
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    private LibVLC _libVLC;
+
+    private MediaPlayer _mediaPlayer;
+
+    private void Stop_Click(object sender, RoutedEventArgs e)
     {
-        private LibVLC _libVLC;
-
-        private MediaPlayer _mediaPlayer;
-
-        private void Stop_Click(object sender, RoutedEventArgs e)
-        {
-            _mediaPlayer.Stop();
-        }
-
-       
-
-        private void Play_Click(object sender, RoutedEventArgs e)
-        {
-            _mediaPlayer.Play();
-        }
-
-        private async void  Snapshot_Click(object sender, RoutedEventArgs e)
-        {
-            //Get path for temporary folder for this app package
-            StorageFolder tempFolder = ApplicationData.Current.TemporaryFolder;
-            //create path and filename for libVLC method
-            var pathstring = tempFolder.Path + "\\tempsnap.png";
-
-            //save snapshot in temporary folder
-            var bb=_mediaPlayer.TakeSnapshot(0,pathstring,0,0);
-            
-            //copy file to Pictures Library
-            StorageFile tempImage = await tempFolder.GetFileAsync("tempsnap.png");
-            StorageFolder picFolder = KnownFolders.PicturesLibrary;
-            StorageFile copiedFile = await tempImage.CopyAsync(picFolder, "snap.png", NameCollisionOption.ReplaceExisting);
-            
-            
+      _mediaPlayer.Stop();
     }
 
-       
 
-        private void Pause_Click(object sender, RoutedEventArgs e)
-        {
-            _mediaPlayer.Pause();
-            
-        }
 
-        public MainPage()
-        {
-            InitializeComponent();
-            Loaded += (s, e) =>
-            {
-                _libVLC = new LibVLC(VideoView.SwapChainOptions);
-                _libVLC.Log += (sender, ee) => Debug.WriteLine($"[{ee.Level}] {ee.Module}:{ee.Message}");
-                _mediaPlayer = new MediaPlayer(_libVLC);
-                VideoView.MediaPlayer = _mediaPlayer;
-                //this._mediaPlayer.Play(new Media(_libVLC, "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4", FromType.FromLocation));
-                //rtsp://b1.dnsdojo.com:1935/live/sys3.stream
-                this._mediaPlayer.Play(new Media(_libVLC, "rtsp://b1.dnsdojo.com:1935/live/sys3.stream", FromType.FromLocation));
-            };
-
-            Unloaded += (s, e) =>
-            {
-                VideoView.MediaPlayer = null;
-                this._mediaPlayer.Dispose();
-                this._libVLC.Dispose();
-               
-            };
-        }
+    private void Play_Click(object sender, RoutedEventArgs e)
+    {
+      _mediaPlayer.Play();
     }
+
+    private async void Snapshot_Click(object sender, RoutedEventArgs e)
+    {
+      //Get path for temporary folder for this app package
+      StorageFolder tempFolder = ApplicationData.Current.TemporaryFolder;
+      //create path and filename for libVLC method
+      //var pathstring = tempFolder.Path + "\\tempsnap.png";
+      var pathstring = tempFolder.Path + "\\tempsnap.jpeg";
+      //save snapshot in temporary folder
+      var bb = _mediaPlayer.TakeSnapshot(0, pathstring, 0, 0);
+
+      //copy file to Pictures Library
+      StorageFile tempImage = await tempFolder.GetFileAsync("tempsnap.jpeg");
+      StorageFolder picFolder = KnownFolders.PicturesLibrary;
+      StorageFile copiedFile = await tempImage.CopyAsync(picFolder, "snap.jpeg", NameCollisionOption.ReplaceExisting);
+
+
+    }
+
+
+
+    private void Pause_Click(object sender, RoutedEventArgs e)
+    {
+      _mediaPlayer.Pause();
+
+    }
+
+    public MainPage()
+    {
+      InitializeComponent();
+      Loaded += (s, e) =>
+      {
+        _libVLC = new LibVLC(VideoView.SwapChainOptions);
+        _libVLC.Log += (sender, ee) => Debug.WriteLine($"[{ee.Level}] {ee.Module}:{ee.Message}");
+        _mediaPlayer = new MediaPlayer(_libVLC);
+        VideoView.MediaPlayer = _mediaPlayer;
+              //this._mediaPlayer.Play(new Media(_libVLC, "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4", FromType.FromLocation));
+              //rtsp://b1.dnsdojo.com:1935/live/sys3.stream
+        //_mediaPlayer.Volume = 0;
+        _mediaPlayer.Play(new Media(_libVLC, "http://video.ch9.ms/ch9/70cc/83e17e76-8be8-441b-b469-87cf0e6a70cc/ASPNETwithScottHunter_high.mp4", FromType.FromLocation));
+        _mediaPlayer.Mute = true;
+        _mediaPlayer.Buffering += (ss, ee) =>
+        {
+          _mediaPlayer.Mute = true;
+         
+        };
+
+      };
+
+      Unloaded += (s, e) =>
+      {
+        VideoView.MediaPlayer = null;
+        this._mediaPlayer.Dispose();
+        this._libVLC.Dispose();
+
+      };
+    }
+
+    private void Vol0_Click(object sender, RoutedEventArgs e)
+    {
+      _mediaPlayer.Mute = true;
+    }
+
+    private void Vol40_Click(object sender, RoutedEventArgs e)
+    {
+      _mediaPlayer.Mute = false;
+    }
+  }
 }
